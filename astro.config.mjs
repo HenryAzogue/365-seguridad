58cf360535b4706245fefbfd398be1fbeeca57d5
Ulhis <hnrazogue@gmail.com>
2026-07-18 00:21:02 +0200

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { defineConfig, envField } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/';
 
 // https://astro.build/config
 export default defineConfig({
